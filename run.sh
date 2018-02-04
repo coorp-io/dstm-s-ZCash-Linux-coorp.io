a7108e980aecaee8553d3f155abb19ca187dac61
coorp.io <post@coorp.io>
2018-02-04 18:19:46 +0300

    init
    init

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!!! Start your miner using the following command ([address] replace with your wallet) !!!!
+#zm --server us.coorp.io --user [address].[worker] --pass x --port 3034
+#ZenCash
+./zm --server eu.coorp.io --user znSNxcGBUZSuhiR3JHtHKygGi35VXgAgdPe.workerName --pass x --port 3032
+#hush
+#./zm --server us.coorp.io --user t1RGwDc21mxd9J511GkQng78j8aFrZPJa3A.workerName --pass x --port 3034
+#VoteCoin
+#./zm --server us.coorp.io --user t1MskC7ubCKUDixmbYL7cLHvdkNJg5aDXpd.workerName --pass x --port 3035
+#BitcoinZ
+#./zm --server us.coorp.io --user t1gUFb4za5jLvpjz7iRc3hPPcFySMdtkKYv.workerName --pass x --port 3033
+#SnowGem
+#./zm --server us.coorp.io --user [address].[worker] --pass x --port 3031
+#Komodo
+#./zm --server eu.coorp.io --user RSCMuFshgvVnf6X4ukVgBHv9zS1sUdr2Ld.workerName --pass x --port 3857
